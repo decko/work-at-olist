5d42dfc88de297f235bf678ec1b38aa11739c152
André  "decko" de Brito <decko@birosca.mobi>
2016-11-29 19:41:05 -0200

    Make channels endpoint return a list. Test passing.

--- work-at-olist/workatolist/channels/models.py
+++ work-at-olist/workatolist/channels/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    """
+    Persist a channel.
+    """
+
+    name = models.CharField('Marketplace name', max_length=100)
--- work-at-olist/workatolist/channels/views.py
+++ work-at-olist/workatolist/channels/views.py
@@ -1,8 +1,11 @@
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Channel
+
 
 class ChannelsView(APIView):
+
     def get(self, request):
-        return Response(status.HTTP_200_OK)
+        channels = [channel.name for channel in Channel.objects.all()]
+        return Response(channels)
