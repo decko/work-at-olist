fdc0bc80053f8b9892e09a8f2df639b187ab7685
André  "decko" de Brito <decko@birosca.mobi>
2016-11-29 17:58:46 -0200

    Returning the right view to /channels/ URL. Test passing.

--- work-at-olist/workatolist/channels/tests.py
+++ work-at-olist/workatolist/channels/tests.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import resolve
+
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -10,3 +12,9 @@ class ChannelsTest(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_channels_endpoint_use_channels_url(self):
+
+        url = resolve('/channels/')
+
+        self.assertEqual(url.view_name, 'channels')
