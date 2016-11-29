5a05a6e2cf009d4e1080a4d17b00df75ea56bea3
André  "decko" de Brito <decko@birosca.mobi>
2016-11-29 17:55:01 -0200

    Return 200 OK to channel URL. Test passing.

--- work-at-olist/workatolist/channels/views.py
+++ work-at-olist/workatolist/channels/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ChannelsView(APIView):
+    def get(self, request):
+        return Response(status.HTTP_200_OK)
--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from workatolist.channels.views import ChannelsView
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^channels/$', ChannelsView.as_view(), name='channels'),
+
 ]
