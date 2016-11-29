7069cd808e370464af2217291761838fef96a268
André  "decko" de Brito <decko@birosca.mobi>
2016-11-29 17:16:32 -0200

    Test the channels endpoint. Test failing!

--- work-at-olist/workatolist/channels/tests.py
+++ work-at-olist/workatolist/channels/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ChannelsTest(APITestCase):
+    def test_channels_endpoint_returning_200_OK(self):
+
+        url = reverse('channels')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
