d18dc915d8bbd2e30853f27d8d0f987100c04e7a
André  "decko" de Brito <decko@birosca.mobi>
2016-11-30 10:11:23 -0200

    Start and configure the categories app

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local apps goes after this line
     'workatolist.channels',
+    'workatolist.categories',
 ]
 
 MIDDLEWARE_CLASSES = [
