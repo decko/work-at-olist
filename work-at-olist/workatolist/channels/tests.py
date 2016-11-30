334ef7a4fb57de3814216c2e8fb5bfe103cc6e56
André  "decko" de Brito <decko@birosca.mobi>
2016-11-30 01:04:52 -0200

    Test endpoint returning channels name and uid. Test passing.

--- work-at-olist/workatolist/channels/tests.py
+++ work-at-olist/workatolist/channels/tests.py
@@ -38,3 +38,13 @@ class ChannelsTest(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_return_channel_name_and_uid(self):
+
+        channel = Channel.objects.create(name="SuperMarketplace")
+
+        url = reverse('channels-detail', kwargs={'uid': channel.uid})
+        response = self.client.get(url)
+
+        self.assertContains(response, channel.uid)
+        self.assertContains(response, channel.name)
