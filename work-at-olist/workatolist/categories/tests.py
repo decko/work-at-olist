fc264fddcec774b68756d9330fdf56607731a9e6
André  "decko" de Brito <decko@birosca.mobi>
2017-01-19 11:44:32 -0200

    Test endpoint categories returning 200 OK. Test failing.

--- work-at-olist/workatolist/categories/tests.py
+++ work-at-olist/workatolist/categories/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CategoriesTest(APITestCase):
+    """
+    Tests for Category model and endpoints
+    """
+
+    def test_categories_endpoint_returning_200_ok(self):
+        """
+        Test for /categories/ url return HTTP 200 ok status
+        """
+
+        url = '/categories/'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
