44b1773294545e421f84dd343d8a82f6712d3e79
André "decko" de Brito <brito.afa@gmail.com>
2017-05-12 06:40:49 -0300

    Refactoring the channels app to use namespaced urls. Tests passing.

--- work-at-olist/workatolist/channels/urls.py
+++ work-at-olist/workatolist/channels/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import ChannelsView
 
 urlpatterns = [
-    url(r'^channels/$', ChannelsView.as_view(), name='channels')
+    url(r'^/$', ChannelsView.as_view(), name='list')
 ]
--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -7,6 +7,8 @@ from workatolist.channels.views import ChannelsDetailView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^channels',
+        include('workatolist.channels.urls', namespace='channels')),
     url(r'^channels/(?P<uid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})', ChannelsDetailView.as_view(), name='channels-detail'),
     url(r'^channels/(?P<slug>[-\w]+)/$', ChannelsDetailView.as_view(),
         name='channels-slug-detail'),
