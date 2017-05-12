2db5774601f6a93698b4deb56252527076bde4fb
André "decko" de Brito <brito.afa@gmail.com>
2017-05-12 06:58:50 -0300

    Finishing url refactor on channels app. Tests passing.

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,20 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from workatolist.channels.views import ChannelsView
-from workatolist.channels.views import ChannelsDetailView
-
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^channels',
         include('workatolist.channels.urls', namespace='channels')),
-    url(r'^channels/(?P<uid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})', ChannelsDetailView.as_view(), name='channels-detail'),
-    url(r'^channels/(?P<slug>[-\w]+)/$', ChannelsDetailView.as_view(),
-        name='channels-slug-detail'),
-    url(r'^channels/$', ChannelsView.as_view(), name='channels'),
-
     url(r'^categories',
-        include('workatolist.categories.urls', namespace='categories')
-       ),
+        include('workatolist.categories.urls', namespace='categories')),
 ]
