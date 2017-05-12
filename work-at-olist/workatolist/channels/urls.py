d741a96707eb2b5d89d3c6dc5c7bfc6cd7bfb0a6
André "decko" de Brito <brito.afa@gmail.com>
2017-05-12 06:46:30 -0300

    Refactoring the detail urls of channels app to use namespaced urls. Tests passing.

--- work-at-olist/workatolist/channels/urls.py
+++ work-at-olist/workatolist/channels/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from .views import ChannelsView
+from .views import ChannelsDetailView
 
 urlpatterns = [
-    url(r'^/$', ChannelsView.as_view(), name='list')
+    url(r'^/$', ChannelsView.as_view(), name='list'),
+    url(r'^channels/(?P<uid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})', ChannelsDetailView.as_view(), name='detail'),
 ]
