ade48517bddb914f13670581862d358ef7d48570
André "decko" de Brito <brito.afa@gmail.com>
2017-05-12 06:57:40 -0300

    Refactoring the detail-slug urls of channels app to use namespaced urls. Tests passing.

--- work-at-olist/workatolist/channels/urls.py
+++ work-at-olist/workatolist/channels/urls.py
@@ -6,4 +6,6 @@ from .views import ChannelsDetailView
 urlpatterns = [
     url(r'^/$', ChannelsView.as_view(), name='list'),
     url(r'^channels/(?P<uid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})', ChannelsDetailView.as_view(), name='detail'),
+    url(r'^channels/(?P<slug>[-\w]+)/$', ChannelsDetailView.as_view(),
+        name='slug-detail'),
 ]
